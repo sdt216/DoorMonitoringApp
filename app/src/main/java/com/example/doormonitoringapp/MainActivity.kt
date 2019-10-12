429ad3fc5574282bf36694f32b4ca76fc7b69cec
sdt216 <sadungtai26@gmail.com>
2019-10-12 11:35:29 +0700

    Added Bluetooth Service for sending Message (still bug)

--- app/src/main/java/com/example/doormonitoringapp/MainActivity.kt
+++ app/src/main/java/com/example/doormonitoringapp/MainActivity.kt
@@ -3,32 +3,109 @@ package com.example.doormonitoringapp
 import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.content.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 
+private const val TAG = "MY_APP_DEBUG_TAG"
+const val MESSAGE_READ: Int = 0
+const val MESSAGE_WRITE: Int = 1
+const val MESSAGE_TOAST: Int = 2
+
+class MyBluetoothService(
+    // handler that gets info from Bluetooth service
+    private val handler: Handler
+) {
+
+    private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
+
+        private val mmInStream: InputStream = mmSocket.inputStream
+        private val mmOutStream: OutputStream = mmSocket.outputStream
+        private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
+
+        override fun run() {
+            var numBytes: Int // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                // Read from the InputStream.
+                numBytes = try {
+                    mmInStream.read(mmBuffer)
+                } catch (e: IOException) {
+                    Log.d(TAG, "Input stream was disconnected", e)
+                    break
+                }
+
+                // Send the obtained bytes to the UI activity.
+                val readMsg = handler.obtainMessage(
+                    MESSAGE_READ, numBytes, -1,
+                    mmBuffer)
+                readMsg.sendToTarget()
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        fun write(bytes: ByteArray) {
+            try {
+                mmOutStream.write(bytes)
+            } catch (e: IOException) {
+                Log.e(TAG, "Error occurred when sending data", e)
+
+                // Send a failure message back to the activity.
+                val writeErrorMsg = handler.obtainMessage(MESSAGE_TOAST)
+                val bundle = Bundle().apply {
+                    putString("toast", "Couldn't send data to the other device")
+                }
+                writeErrorMsg.data = bundle
+                handler.sendMessage(writeErrorMsg)
+                return
+            }
+
+            // Share the sent message with the UI activity.
+            val writtenMsg = handler.obtainMessage(
+                MESSAGE_WRITE, -1, -1, mmBuffer)
+            writtenMsg.sendToTarget()
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
     var m_bluetoothAdapter: BluetoothAdapter? = null
-
     lateinit var m_pairedDevices: Set<BluetoothDevice>
     val REQUEST_ENABLE_BLUETOOTH = 1
     var adapter : MyBluetoothAdapter? = null
-    val deviceNameList: MutableList<String>? = mutableListOf<String>()
-    val deviceMAC: MutableList<String>? = mutableListOf<String>()
-
-    companion object {
-        val EXTRA_ADDRESS: String = "Device_address"
+    val handler: Handler = Handler {
+        true
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val serverSocket: BluetoothServerSocket?
         var BTAdapter = BluetoothAdapter.getDefaultAdapter()
         // Phone does not support Bluetooth so let the user know and exit.
         if (BTAdapter == null) {
@@ -40,99 +117,22 @@ class MainActivity : AppCompatActivity() {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show()
         }
-
         if (!BTAdapter.isEnabled) {
             val enableBT = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBT, REQUEST_ENABLE_BLUETOOTH)
-        }
-
-
-
-//
-//        val m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-////
-////
-////        if (m_bluetoothAdapter != null) {
-////            m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
-////        }
-////
-//        if (adapter == null) {
-//            adapter = MyBluetoothAdapter()
-//        }
-//        bluetoothDeviceList?.adapter = adapter
-//        adapter?.setList(m_pairedDevices)
-
-        refreshDeviceList?.setOnClickListener {
-            val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-            registerReceiver(receiver, filter)
-
-            bluetoothDeviceList?.adapter = adapter
-            adapter?.setList(BluetoothDevice.)
-            m_bluetoothAdapter?.startDiscovery()
-            if (m_bluetoothAdapter == null) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH)
-            } else {
-
-                m_bluetoothAdapter?.startDiscovery()
-                m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
-                (bluetoothDeviceList?.adapter as? MyBluetoothAdapter)?.let { myAdapter ->
-                    myAdapter.setList(m_pairedDevices)
-                }
+        } else {
+            connectBluetooth?.setOnClickListener {
+                val intentOpenBluetoothSettings = Intent()
+                intentOpenBluetoothSettings.action =
+                    android.provider.Settings.ACTION_BLUETOOTH_SETTINGS
+                startActivity(intentOpenBluetoothSettings)
             }
-        }
 
-//            Toast.makeText(applicationContext, "$position - ${bluetoothDevice.name}", Toast.LENGTH_SHORT).show()
-
-//            m_bluetoothAdapter?.startDiscovery()
-//        }
-////        refreshDeviceList?.setOnClickListener {
-////            val m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-////            m_bluetoothAdapter?.startDiscovery()
-////            if (m_bluetoothAdapter.isEnabled) {
-////                if (m_bluetoothAdapter != null) {
-////                    m_bluetoothAdapter?.startDiscovery()
-////
-//////                    m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
-//////                    (bluetoothDeviceList?.adapter as? MyBluetoothAdapter)?.let { myAdapter ->
-//////                        myAdapter.setList(m_pairedDevices)
-////
-////                }
-////            }
-////            else {
-////
-////                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-////                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH)
-////            }
-////
-////
-////        }
-    }
-
-    private val receiver = object : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            val action: String? = intent.action
-            when(action) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    // Discovery has found a device. Get the BluetoothDevice
-                    // object and its info from the Intent.
-                    val device: BluetoothDevice =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device.name
-                    val deviceHardwareAddress = device.address // MAC address
-                }
-            }
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
 
 
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(receiver)
-    }
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
 
         return super.onCreateView(name, context, attrs)
